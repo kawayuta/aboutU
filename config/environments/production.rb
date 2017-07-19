df9ffa811b84916c0b5cacb6d00044e4b523fbba
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-07-19 13:25:53 +0900

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+config.root = Rails.root.join('tmp')
+config.cache_dir = "#{Rails.root}/tmp/uploads"
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
